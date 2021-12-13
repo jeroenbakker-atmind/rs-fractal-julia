851b324dfe557f0d270f7ea2342c2b51ce209bcc
Jeroen Bakker <j.bakker@atmind.nl>
2021-12-13 18:51:57 +0100

    Separate into two loops.

--- src/julia.rs
+++ src/julia.rs
@@ -10,72 +10,98 @@ pub struct Julia {
 impl Julia {
     pub fn generate<T>(&self, buffer: &mut dyn BufferTrait)
     where
-        T: JuliaSample,
+        T: JuliaRow,
     {
         let r2 = self.r * self.r;
 
-        let width = buffer.get_width();
         let height = buffer.get_height();
         let backend = T::default();
 
-        let mut offset = 0;
         for y in 0..height {
-            for x in 0..width {
-                let zx = x as f64 / width as f64 - 0.5;
-                let zy = y as f64 / height as f64 - 0.5;
-
-                let iteration = backend.coord_stablization(self, zx, zy, r2);
-                if iteration == self.max_iteration {
-                    offset += 1;
-                    continue;
-                }
-
-                let value = iteration as f32 / self.max_iteration as f32;
-                buffer.store_pixel(offset, value);
-                offset += 1;
-            }
+            backend.julia_row(self, buffer, y, r2);
             print!("{}/{}\r", y, height);
         }
     }
 }
 
-pub trait JuliaSample: Default {
-    fn coord_stablization(&self, julia: &Julia, x: f64, y: f64, r2: f64) -> usize;
-}
-
-impl JuliaSample for f64 {
-    fn coord_stablization(&self, julia: &Julia, x: f64, y: f64, r2: f64) -> usize {
-        let mut x = x;
-        let mut y = y;
-        let mut iteration = 0;
-        let max_iteration = julia.max_iteration;
-        let cx = julia.cx as f64;
-        let cy = julia.cy as f64;
-        while x * x + y * y < r2 && iteration < max_iteration {
-            let xtemp = x * x - y * y;
-            y = 2.0 * x * y + cy;
-            x = xtemp + cx;
-            iteration += 1;
+pub trait JuliaRow: Default {
+    fn julia_row(&self, julia: &Julia, buffer: &mut dyn BufferTrait, row: u32, r2: f64);
+    fn store_pixel(
+        &self,
+        iteration: usize,
+        max_iteration: usize,
+        buffer: &mut dyn BufferTrait,
+        offset: usize,
+    ) {
+        if iteration == max_iteration {
+            return;
         }
-        iteration
+        let value = iteration as f32 / max_iteration as f32;
+        buffer.store_pixel(offset, value);
     }
 }
 
-impl JuliaSample for f32 {
-    fn coord_stablization(&self, julia: &Julia, x: f64, y: f64, r2: f64) -> usize {
-        let mut x = x;
-        let mut y = y;
-        let mut iteration = 0;
+impl JuliaRow for f64 {
+    fn julia_row(&self, julia: &Julia, buffer: &mut dyn BufferTrait, row: u32, r2: f64) {
         let max_iteration = julia.max_iteration;
-        let cx = julia.cx as f64;
-        let cy = julia.cy as f64;
-        while x * x + y * y < r2 && iteration < max_iteration {
-            let xtemp = x * x - y * y;
-            y = 2.0 * x * y + cy;
-            x = xtemp + cx;
-            iteration += 1;
+        let height = buffer.get_height();
+        let width = buffer.get_width();
+
+        let mut samples = vec![0_usize; width as usize];
+        for x in 0..width as usize {
+            let mut zx = x as f64 / width as f64 - 0.5;
+            let mut zy = row as f64 / height as f64 - 0.5;
+
+            let mut iteration = 0;
+            let cx = julia.cx as f64;
+            let cy = julia.cy as f64;
+            while zx * zx + zy * zy < r2 && iteration < max_iteration {
+                let xtemp = zx * zx - zy * zy;
+                zy = 2.0 * zx * zy + cy;
+                zx = xtemp + cx;
+                iteration += 1;
+            }
+
+            samples[x] = iteration;
         }
-        iteration
+
+        let mut offset = width as usize * row as usize;
+        for sample_offset in 0..width as usize {
+            self.store_pixel(samples[sample_offset], max_iteration, buffer, offset);
+            offset += 1;
+        }
     }
 }
 
+impl JuliaRow for f32 {
+    fn julia_row(&self, julia: &Julia, buffer: &mut dyn BufferTrait, row: u32, r2: f64) {
+        let r2 = r2 as f32;
+        let max_iteration = julia.max_iteration;
+        let height = buffer.get_height();
+        let width = buffer.get_width();
+
+        let mut samples = vec![0_usize; width as usize];
+        for x in 0..width as usize {
+            let mut zx = x as f32 / width as f32 - 0.5;
+            let mut zy = row as f32 / height as f32 - 0.5;
+
+            let mut iteration = 0;
+            let cx = julia.cx as f32;
+            let cy = julia.cy as f32;
+            while zx * zx + zy * zy < r2 && iteration < max_iteration {
+                let xtemp = zx * zx - zy * zy;
+                zy = 2.0 * zx * zy + cy;
+                zx = xtemp + cx;
+                iteration += 1;
+            }
+
+            samples[x] = iteration;
+        }
+
+        let mut offset = width as usize * row as usize;
+        for sample_offset in 0..width as usize {
+            self.store_pixel(samples[sample_offset], max_iteration, buffer, offset);
+            offset += 1;
+        }
+    }
+}
