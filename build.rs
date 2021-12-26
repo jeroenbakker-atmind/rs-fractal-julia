310b6b0ddcd5066695873ba43b2804cf096def1b
Jeroen Bakker <j.bakker@atmind.nl>
2021-12-26 22:06:23 +0100

    Use intel syntax.

--- build.rs
+++ build.rs
@@ -2,5 +2,10 @@ extern crate cc;
 
 fn main() {
     println!("cargo:rerun-if-changed=src/julia-asm.S");
-    cc::Build::new().file("src/julia-asm.S").compile("juliaasm");
+    cc::Build::new()
+        .flag("-Wa,-mmnemonic=intel")
+        .flag("-Wa,-msyntax=intel")
+        .flag("-Wa,-mnaked-reg")
+        .file("src/julia-asm.S")
+        .compile("juliaasm");
 }
